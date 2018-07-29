7f077ef878747e2e24c3073d8858118af302ccf5
iamNitin16 <nitinsharma95ns@gmail.com>
2018-07-29 15:58:50 +0530

    added Cards and fetch data from server

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import Card from './Card';
+
+export default class AlbumDetail extends Component {
+  render() {
+    return (
+      <Card>
+        <Text>{this.props.album.title}</Text>
+      </Card>
+    );
+  }
+}
---
+++ src/components/CardSection.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export default class CardSection extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  containerStyle: {
+    borderBottomWidth: 1,
+  }
+});
